12736d3b58d8ed1654aba308c9b453f53cfdcdca
Jose Manuel <josemhp709@gmail.com>
2018-10-28 03:01:03 -0600

    Factorizacion al 90%

--- Proyecto.rkt
+++ Proyecto.rkt
@@ -160,9 +160,6 @@
     (if (= (car L) 0) (pos-Pco (cdr L) (+ pos 1))
         pos)))
 
-(define imprimo
-  (lambda (L)
-    L))
 (define susDiv1
   (lambda (dividiendo cociente divisor) ; Hace el nuevo dividiendo , dividiendo -(cociente * divisor) ;Primero se hace susDiv2
   (resta-doble dividiendo (mult1 (primerCociente cociente) (posPrimerCociente cociente) divisor))))   ;Practicamente es el residuo.
@@ -245,6 +242,7 @@
     (suma-lista (map * lista (listaExponentes lista x)))))
 
 ;Factorizacion--------------------------------
+;Grado 2.----------------------------
 (define discriminante
   (lambda (lista)
     (- (exponente (cadr lista) 2) (* 4 (car lista) (caddr lista)))))
@@ -255,6 +253,109 @@
   ))
 (define formula2
   (lambda (lista)
-    (/(-(cadr lista) (sqrt(discriminante lista))) (* 2(caddr lista)))
+    (/(-(-(cadr lista)) (sqrt(discriminante lista))) (* 2(caddr lista)))
     ))
 
+(define grado2 ;Tiene que ser una lista de tam 3
+  (lambda (lista)
+    (if (and (=(length lista) 3) (not(eq? (caddr lista) 0)))
+    (list (list (-(formula lista)) 1) (list (*(caddr lista) (-(formula2 lista))) (*(caddr lista)1)))
+    '()
+    )))
+;GRADO 3--------------------------------------------
+(define divisores-positivos
+ (lambda (n)
+   (if (>= n 1)
+       (divisores n n)
+       (divisores (* -1 n) (* -1 n)))))
+(define divisores-negativos
+ (lambda(n)
+   (if (>= n 1)
+       (map - (divisores n n))
+       (map - (divisores (* -1 n) (* -1 n))))))
+(define divisores-PN ;Esto une los divisores negativos y positivos del elemento del ultimo elemento del polininomio (El que no tiene x)
+  (lambda (n)
+    (append (divisores-positivos n) (divisores-negativos n))))
+ 
+(define divisores
+  (lambda (n div)
+    (if (> div 0)
+        (if (integer? (/ n div)) (cons div (divisores n (- div 1)))
+            (divisores n (- div 1)))
+        '())))
+;Dividir los divisores del ultimo elemento contra los divisores del primero. (Del primero solo ocupo los positivos.
+(define divide-N-Lista
+  (lambda (lista n)
+    (if (null? lista) '()
+        (cons (/ (car lista) n) (divide-N-Lista (cdr lista) n)))))
+
+(define posibles-raices
+  (lambda (lista)
+    (if (null? lista) '()
+        (posibles-raices2  (divisores-PN (car lista)) (divisores-positivos (ultimoLista lista))))))
+
+(define posibles-raices2
+  (lambda (listaA listaB) ;Dividir Cada car de listaB por todos los de lista A
+    (if(null? listaB) '()
+       (append (divide-N-Lista listaA (car listaB)) (posibles-raices2 listaA (cdr listaB))))))
+;------------Invertir la lista para poder hacer la division sintetica------------------
+(define concatenar
+  (lambda (l1 l2)
+    (cond ((null? l1) l2)
+          (else (cons (car l1)
+                      (concatenar (cdr l1) l2))))))
+;; Funcion invertir
+;; invierte una lista
+(define invertir
+  (lambda (l)
+    (cond ((null? l) l)
+          (else (concatenar (invertir (cdr l))
+                            (list (car l)))))))
+;------------DIVISION SINTETICA---------
+(define div-s
+  (lambda (lista)
+    (div-s2 (invertir lista) (posibles-raices lista)))) ; HAY que invertir la lista para poder aplicar la division sintetica.
+
+(define div-s2
+  (lambda (listaInvertida raices)
+    (if(null? raices)  (display '(El polinomio no tiene raices reales))
+    (if (null? (div-s3 (cdr listaInvertida) (car listaInvertida) (car raices)))
+        (div-s2 listaInvertida (cdr raices))
+        (car raices)))))
+
+(define div-s3
+  (lambda (lista primero x) ;Este metodo recibe: (cdr de lista) (car lista) y X que es un elemento de la raiz.
+    (if (>(length lista) 1) ; Osea todavia no estamos en el ultimo elemento.
+        (div-s3 (cdr lista)(+(* primero x) (car lista)) x)
+        (if (=(+ (car lista) (* x primero)) 0) x ; Esta es la raiz :D
+            '()))))
+(define eliminaFinal
+  (lambda (L)
+    (if(null? L) '()
+       (if (=(length L) 1)  '()
+           (cons (car L) (eliminaFinal (cdr L)))))))
+
+(define div-s11 ;Crea el polinomio de grado 2, que da como resultado de la division sintetica.
+  (lambda (lista x) ;Ya va a recibir la cola de lista, el numero y el X
+    (cons (car (invertir lista))(div-s22 (cdr(invertir lista)) (car (invertir lista)) x)))) ; HAY que invertir la lista para poder aplicar la division sintetica.
+
+(define div-s22 
+  (lambda (lista primero x)
+    (if(>(length lista) 1)
+       (cons (+ (* primero x) (car lista)) (div-s22 (cdr lista) (+ (* primero x) (car lista)) x))
+       '())))
+
+(define grado3 ;Tiene que ser una lista de tam 4
+  (lambda (lista)
+    (if (and (=(length lista) 4) (not(eq? (cadddr lista) 0)))
+        (if (integer? (div-s lista))
+            (cons (list (-(div-s lista)) 1 ) (grado2 (div-s11 lista (div-s lista))))
+             (display '(El polinomio no tiene raices reales)))
+         (display '(El polinomio no cumple con los requisitos)))))
+
+;GRADOS SUPERIORES-----------------------------------------
+(define gradoN
+  (lambda (lista)
+    (cond ((=(length lista) 3) (grado2 lista))
+          ((=(length lista) 4) (grado3 lista))
+          (else (cons (list (-(div-s lista)) 1 ) (gradoN(div-s11 lista (div-s lista))))))))
